b5b6b43a3957eb147c551a2c8bb5017c77d3955d
codarel <codarel.id@gmail.com>
2021-03-21 15:21:21 +0700

    validasi login

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
+// validasi login username=codarel password=codarel123
+function loginValidate() {
+  const attempt = 3;
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+  if (username == "codarel" && password == "codarel123") {
+    alert("Login berhasil!");
+    window.location = "home.html";
+    return false;
+  } else {
+    attempt--;
+    alert(`Login gagal, kamu punya ${attempt} kesempatan untuk mencoba lagi!`);
+    if (attempt == 0) {
+      document.getElementById("username").disabled = true;
+      document.getElementById("password").disabled = true;
+      document.getElementById("submit").disabled = true;
+      return false;
+    }
+  }
+}
+
+// membuka tab pada halaman home dan store
 function openTab(evt, tabName) {
   let i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
